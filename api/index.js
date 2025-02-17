16d82f63882080eb63319e4831a68a9e13006e25
salmanshahid5 <salmanshahid0333@gmail.com>
2025-02-17 06:55:56 +0500

    messages controller, routes, mmodel create

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import gigRoute from "./routes/gig.route.js"
 import reviewRoute from "./routes/review.route.js"
 import orderRoute from "./routes/order.route.js"
 import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
@@ -32,6 +33,7 @@ app.use("/api/gigs", gigRoute);
 app.use("/api/reviews", reviewRoute);
 app.use("/api/orders",orderRoute)
 app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
